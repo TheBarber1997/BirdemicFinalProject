11d2d9e1e17ac481750a90d2c81d97741d220b89
PeterBarber <PeterBarber@kentisd.com>
2015-03-18 11:27:43 -0400

    tons of changes

--- Webpages/HomePage(2).aspx.cs
+++ Webpages/HomePage(2).aspx.cs
@@ -5,13 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class MasterPages_MasterPage : System.Web.UI.MasterPage
+public partial class Webpages_Home_Page2_ : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
-    {
-
-    }
-    protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
     {
 
     }
